809a906cdec1a9c7ea98e3d3fef38954666596c5
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-10 17:55:19 -0400

    Initial commit 🚀🎉

---
+++ resources/assets/components/ExpandableText.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <p :class="{ contracted: contracted }">
+            <slot></slot>
+        </p>
+        <button v-if="contracted" class="more" @click="contracted = false">+ More</button>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                contracted: true
+            }
+        }
+    }
+</script>
+<style>
+    .about p {
+        white-space: pre-wrap;
+    }
+
+    .about p.contracted {
+        height: 250px;
+        overflow: hidden;
+    }
+
+    .about button.more {
+        background: transparent;
+        border: 0;
+        color: #008489;
+        padding: 0;
+        font-size: 17px;
+        font-weight: bold;
+    }
+
+    .about button.more:hover,
+    .about button.more:focus,
+    .about button.more:active {
+        text-decoration: underline;
+        outline: none;
+    }
+</style>
