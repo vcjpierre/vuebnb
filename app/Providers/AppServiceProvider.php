7836b9c242d6c58d7f923fae5e1e9a4892dbf082
Jean Pierre Velez <vcjpierre@gmail.com>
2020-03-23 14:45:56 -0400

    Propare to deploy 🚀

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        If (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+        
         Schema::defaultStringLength(191);
     }
 
